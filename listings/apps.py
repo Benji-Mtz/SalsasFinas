e84339e1f91cec13f210f5c9ff47744ce38a4f55
Benji Martinez Flores <49083076+Benji-Mtz@users.noreply.github.com>
2022-02-04 19:00:06 -0600

    first models

---
+++ listings/apps.py
@@ -0,0 +1,6 @@
+# Includes the main configuration of the listings applications.
+from django.apps import AppConfig
+
+
+class ListingsConfig(AppConfig):
+    name = 'listings'
---
+++ listings/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+"""
+will contain the business logic of our application. Each view receives an HTTP request, processes it, and
+returns a response. This file determines how data is processed and shown to the user.
+"""
